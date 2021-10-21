ec2d56779287f21de07a83dc9ff1ee21fd729f67
RainbowGravity <reinerrubin2020@gmail.com>
2021-10-21 05:26:13 +0300

    Golang app

---
+++ golang-app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World 2.3")
+	})
+	r.POST("/", func(c *gin.Context) {
+		c.String(200, "Hello World 2.3")
+	})
+	err := r.Run(":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+}
